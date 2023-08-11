a21abac7fe697d435cff1ab458bb94fb9706f36d
yuekcc <yuekcc@qq.com>
2023-08-11 22:59:04 +0800

    clean

--- make-index.js
+++ make-index.js
@@ -1,11 +1,21 @@
-import fs from 'node:fs/promises'
+import fs from 'node:fs/promises';
 
-const list = await fs.readdir('./news')
+const list = await fs.readdir('./news');
 // console.log(list)
 
 const result = [];
-list.reverse().forEach(name => {
-    result.push(`- [${name.replaceAll(/\.md$/g, '')}](./news/${name})`)
-})
+list.reverse().forEach((name) => {
+  if (name === 'catalogue.json') {
+    return;
+  }
 
-await fs.writeFile('list.md', result.join('\n'), 'utf-8')
+  result.push(`- [${name.replaceAll(/\.md$/g, '')}](./news/${name})`);
+});
+
+const content = `# 目录
+
+<!-- INSERT -->
+${result.join('\n')}
+`;
+
+await fs.writeFile('INDEX.md', content, 'utf-8');
