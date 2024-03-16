9af13cd53a4a4eea8563978bd6e4e8259ce8c7e3
yuekcc <yuekcc@qq.com>
2024-03-17 01:34:37 +0800

    使用 UTC+8 时间

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import { fetchNews } from './fetch_news.js';
 
+
+const today = new Date(); // 本地时间
+
+// UTC+8 时间
+const cnToday = new Date(
+  Date.UTC(
+    today.getUTCFullYear(),
+    today.getUTCMonth(),
+    today.getUTCDate(),
+    today.getUTCHours(),
+    today.getUTCMinutes() + 480,
+    today.getUTCSeconds(),
+  ),
+);
+const yesterday = new Date(cnToday.getFullYear(), cnToday.getMonth(), cnToday.getDate() - 1, 12, 0, 0);
+
 // 获取昨天的数据
-const today = new Date();
-const yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1, 12, 0, 0);
 fetchNews(yesterday);
