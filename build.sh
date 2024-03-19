23d77e8e87455ba2603ba18adde095d59445a5ad
yuekcc <yuekcc@qq.com>
2024-03-19 23:48:43 +0800

    不再需要build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bun x esbuild index.js --format=esm --platform=node --target=node18 --bundle --minify --outdir=dist
-bun x esbuild make-index.js --format=esm --platform=node --target=node18 --bundle --minify --outdir=dist
-
