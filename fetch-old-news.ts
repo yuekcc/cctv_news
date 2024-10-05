755d09b7dbe211923dafc246ff09554a4a05ab78
yuekcc <yuekcc@qq.com>
2024-10-05 15:15:42 +0800

    styles

--- fetch-old-news.ts
+++ fetch-old-news.ts
@@ -1,24 +1,25 @@
 import { fetchNews } from './_fetch-news.ts';
 
-let startDate = 1;
-const startMonth = 0;
-const startYear = 2022;
-let thatDate = new Date(startYear, startMonth, startDate);
+const START_MONTH = 0;
+const START_YEAR = 2022;
+
 const today = new Date();
+let date = 1;
+let thatDate = new Date(START_YEAR, START_MONTH, date);
 
 async function fetchHistory() {
-	try {
-		while (thatDate.getTime() < today.getTime()) {
-			await fetchNews(thatDate);
-			await new Promise((resolve) => setTimeout(resolve, 1000));
+  try {
+    while (thatDate.getTime() < today.getTime()) {
+      await fetchNews(thatDate);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
 
-			startDate++;
-			thatDate = new Date(startYear, startMonth, startDate);
-		}
-	} catch (err) {
-		console.warn('下载失败，', (err as Error).message);
-		process.exit(1);
-	}
+      date++;
+      thatDate = new Date(START_YEAR, START_MONTH, date);
+    }
+  } catch (err) {
+    console.warn('下载失败，', (err as Error).message);
+    process.exit(1);
+  }
 }
 
 fetchHistory();
--- make-index.ts
+++ make-index.ts
@@ -4,76 +4,76 @@ import prettier from 'prettier';
 const mdList = await fs.readdir('./news');
 
 function printLink(name?: string | null) {
-	if (!name) {
-		return '';
-	}
+  if (!name) {
+    return '';
+  }
 
-	return `[${name.replaceAll(/\.md$/g, '')}](./news/${name}.md)`;
+  return `[${name.replaceAll(/\.md$/g, '')}](./news/${name}.md)`;
 }
 
 function chunk<T>(store: T[], size: number) {
-	const groupedList = [];
-	for (let i = 0; i < store.length; i += size) {
-		groupedList.push(store.slice(i, i + size));
-	}
+  const groupedList = [];
+  for (let i = 0; i < store.length; i += size) {
+    groupedList.push(store.slice(i, i + size));
+  }
 
-	return groupedList;
+  return groupedList;
 }
 
 const groupedByMonth = mdList
-	.filter((name) => name !== 'catalogue.json')
-	.map((name) => name.replaceAll(/\.md$/g, ''))
-	.reduce(
-		(g, it) => {
-			const key = it.substring(0, 6); // 年月，如：202302
-			const list = g[key] || [];
-			list.push(it);
-			g[key] = list;
+  .filter((name) => name !== 'catalogue.json')
+  .map((name) => name.replaceAll(/\.md$/g, ''))
+  .reduce(
+    (g, it) => {
+      const key = it.substring(0, 6); // 年月，如：202302
+      const list = g[key] || [];
+      list.push(it);
+      g[key] = list;
 
-			return g;
-		},
-		{} as Record<string, string[]>,
-	);
+      return g;
+    },
+    {} as Record<string, string[]>,
+  );
 
 let buf = '# 目录\n\n';
 
 const mapGroupedByYearMonth = Object.entries(groupedByMonth).toSorted((a, b) => {
-	return Number.parseInt(b[0]) - Number.parseInt(a[0]);
+  return Number.parseInt(b[0]) - Number.parseInt(a[0]);
 });
 
 for (const [label, newsList] of mapGroupedByYearMonth) {
-	buf += `## ${label}\n\n`;
+  buf += `## ${label}\n\n`;
 
-	buf += '| 一 | 二 | 三 | 四 | 五 | 六 | 日 |\n';
-	buf += '|---|---|---|---|---|---|---|\n';
+  buf += '| 一 | 二 | 三 | 四 | 五 | 六 | 日 |\n';
+  buf += '|---|---|---|---|---|---|---|\n';
 
-	// 每月的列表
-	const sortedNewsList: Array<string | null> = newsList.toSorted((a, b) => Number.parseInt(a) - Number.parseInt(b));
+  // 每月的列表
+  const sortedNewsList: Array<string | null> = newsList.toSorted((a, b) => Number.parseInt(a) - Number.parseInt(b));
 
-	// 在开关补充一些 null，对齐日期、星期
-	{
-		const fst = sortedNewsList[0] || '';
+  // 在开关补充一些 null，对齐日期、星期
+  {
+    const fst = sortedNewsList[0] || '';
 
-		const firstDay = new Date(
-			Number.parseInt(fst.substring(0, 4)),
-			Number.parseInt(fst.substring(4, 6)) - 1,
-			Number.parseInt(fst.substring(6)),
-			0,
-			0,
-			0,
-		).getDay();
+    const firstDay = new Date(
+      Number.parseInt(fst.substring(0, 4)),
+      Number.parseInt(fst.substring(4, 6)) - 1,
+      Number.parseInt(fst.substring(6)),
+      0,
+      0,
+      0,
+    ).getDay();
 
-		for (let i = 0; i < firstDay - 1; i++) {
-			sortedNewsList.unshift(null);
-		}
-	}
+    for (let i = 0; i < firstDay - 1; i++) {
+      sortedNewsList.unshift(null);
+    }
+  }
 
-	buf += chunk(sortedNewsList, 7)
-		.map((dates) => `| ${dates.map(printLink).join(' | ')} |`)
-		.map((row) => `${row}\n`)
-		.join('');
+  buf += chunk(sortedNewsList, 7)
+    .map((dates) => `| ${dates.map(printLink).join(' | ')} |`)
+    .map((row) => `${row}\n`)
+    .join('');
 
-	buf += '\n\n';
+  buf += '\n\n';
 }
 
 const formatted = await prettier.format(buf, { parser: 'markdown' });
